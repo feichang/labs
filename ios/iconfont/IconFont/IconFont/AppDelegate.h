bb2e3291efecb2781277dd3d19d0bef90e43fcdb
feichang.wyl <feichang.wyl@taobao.com>
2014-01-06 11:15:50 +0800

    Initial Commit

---
+++ ios/iconfont/IconFont/IconFont/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IconFont
+//
+//  Created by Allen Wang on 14-1-6.
+//  Copyright (c) 2014年 taobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/iconfont/IconFont/IconFont/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IconFont
+//
+//  Created by Allen Wang on 14-1-6.
+//  Copyright (c) 2014年 taobao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
