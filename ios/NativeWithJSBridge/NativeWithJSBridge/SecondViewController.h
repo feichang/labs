ff2b70914fc827c41886978b4c2e7d71db2899aa
feichang.wyl <feichang.wyl@taobao.com>
2014-01-06 15:18:03 +0800

    Initial Commit

---
+++ ios/NativeWithJSBridge/NativeWithJSBridge/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  NativeWithJSBridge
+//
+//  Created by Allen Wang on 14-1-6.
+//  Copyright (c) 2014年 taobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
